df0fdb6aa91cc8240334daadbe435630230fda0b
luckyligovi <lucky.luvihi@student.moringaschool.com>
2024-06-05 09:10:08 +0300

    created the attributes setters and getters

---
+++ Student.java
@@ -0,0 +1,41 @@
+public class Student {
+    // attributes
+    private String id;
+    private double gpa;
+    private String name;
+
+    // constructor
+    public Student(String id, double gpa, String name) {
+        this.id = id;
+        this.gpa = gpa;
+        this.name = name;
+
+    }
+
+    // getters and setters methods 
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+
+    }
+
+    public double getGpa() {
+        return this.gpa;
+    }
+
+    public void setGpa(double gpa) {
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+
+    }
+
+}
